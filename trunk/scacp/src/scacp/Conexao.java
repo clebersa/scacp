b7aa3518a524069b0269e94d7213204cfa04b49b
cleber.93cd <cleber.93cd@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-12 05:20:53 +0000

    Construção e configuração da classe Conexao, especificando o uso do HSQLDB.

--- trunk/scacp/src/scacp/Conexao.java
+++ trunk/scacp/src/scacp/Conexao.java
@@ -4,10 +4,31 @@
  */
 package scacp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author cleber
  */
 public class Conexao {
+    private static Connection conexao; // Objeto que será utilizado para a conexão com o banco de dados
+    private static final String JDBC_DRIVER = "org.hsqldb.jdbcDriver"; // Driver do banco de dados utilizado, permitindo conectar-se a ele
+    private static final String URL = "jdbc:hsqldb:file://home/cleber/Dropbox/NetbeansProjects/scacp/src/scacp/SCACP_DB"; // Endereço do banco de dados ao qual você vai se conectar
+    private static final String USUARIO = "SA";// Usuário que vai conectar-se ao banco
+    private static final String SENHA = "";// Senha do usuário para se conectar ao banco
     
+    public static Connection getConexao(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            return conexao;
+        } catch (SQLException excecao) {
+            System.out.println("Problemas de conexão com URL, usuário e/ou senha!");
+        } catch (ClassNotFoundException excecao) {
+            System.out.println("Driver não encontrado!");
+        }
+        return null;
+    }
 }

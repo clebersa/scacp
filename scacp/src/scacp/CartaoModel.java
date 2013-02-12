d8a34ced83df9c307ec0b2f06fb5e2a068359649
leticianb1 <leticianb1@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-12 21:51:18 +0000

    Definicão das Classes - Criação das variáveis

--- scacp/src/scacp/CartaoModel.java
+++ scacp/src/scacp/CartaoModel.java
@@ -9,5 +9,9 @@ package scacp;
  * @author cleber
  */
 public class CartaoModel {
-    
+    int numero_inscricao;
+    int tipo_prova;
+    int fk_id_prova;
+    char questoes_marcadas[] = new char[100];
+    double nota;
 }
--- scacp/src/scacp/ProvaModel.java
+++ scacp/src/scacp/ProvaModel.java
@@ -9,5 +9,14 @@ package scacp;
  * @author cleber
  */
 public class ProvaModel {
+    int id_prova;
+    int tipo_prova;
+    int quantidade_questoes;
+    double pontuacao_minima;
+    double pontuacao_maxima;
+    int precisao_pontuacao;
+    int incidencia_penalizacao;
+    int proporcao_penalizacao;
+    char gabarito[] = new char[100];
     
 }

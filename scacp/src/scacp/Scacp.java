cb0171d3662295feaba02715ab4f398efe884fd6
cleber.93cd <cleber.93cd@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-12 16:19:02 +0000

    Construção e configuração da classe Conexao, especificando o uso do HSQLDB.

--- scacp/src/scacp/Scacp.java
+++ scacp/src/scacp/Scacp.java
@@ -4,6 +4,11 @@
  */
 package scacp;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author cleber
@@ -14,6 +19,11 @@ public class Scacp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection conexao = Conexao.getConexao();
+        try {
+            System.out.println(conexao.getCatalog());
+        } catch (SQLException ex) {
+            Logger.getLogger(Scacp.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }

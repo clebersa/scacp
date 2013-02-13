29c4231b654372bf82e9a00f6d0472b8d352a178
larissamean <larissamean@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-13 22:10:24 +0000

    Atualização das classes DAO e Model

--- scacp/src/scacp/CartaoDAO.java
+++ scacp/src/scacp/CartaoDAO.java
@@ -4,10 +4,48 @@
  */
 package scacp;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Larissa
  */
 public class CartaoDAO {
+    public void inserirCartao(Cartao cartao) {
+        Connection conexao = Conexao.getConexao();
+        try {
+            PreparedStatement st = conexao.prepareStatement("insert into cartoes (numero_inscricao, fk_id_prova, marcacao, nota) values (?,?,?,?)");
+            st.setInt(1, cartao.getNumeroInscricao());
+            st.setInt(2, cartao.getIdProva());
+            st.setString(3, cartao.getMarcacao());
+            st.setDouble(4, cartao.getNota());
+            st.execute();
+            st.close();
+        } catch (SQLException excecao) {
+            JOptionPane.showMessageDialog(null, "Erro ao criar o statement!");
+        }
+    }
     
+    public void alterarCartao(Cartao cartao){
+        // Passos para alteração de cartão
+    }
+    
+    public void buscarCartao(int numeroInscricao){
+        // Passos para localização de cartão
+    }
+    
+    public void excluirCartao(int numeroInscricao) {
+        Connection conexao = Conexao.getConexao();
+        try {
+            PreparedStatement st = conexao.prepareStatement("delete from cartoes where numero_inscricao = ?");
+            st.setInt(1, numeroInscricao);
+            st.execute();
+            st.close();
+        } catch (SQLException excecao) {
+            JOptionPane.showMessageDialog(null, "Erro ao tentar deletar");
+        }
+    }
 }
--- scacp/src/scacp/ProvaDAO.java
+++ scacp/src/scacp/ProvaDAO.java
@@ -9,5 +9,19 @@ package scacp;
  * @author Larissa
  */
 public class ProvaDAO {
+    public void gravarProva(Prova prova){
+        //Passos para gravar uma prova
+    }
     
+    public void alterarProva(Prova prova){
+        //Passos para alterar uma prova
+    }
+    
+    public void buscarProva(int idProva){
+        //Passos para buscar uma prova
+    }
+    
+    public void excluirProva(int idProva){
+        //Passos para excluir uma prova
+    }
 }

d0365b81e09d4a1934da1d7f13189f190eb52fc7
larissamean <larissamean@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-20 02:34:19 +0000

    Arrumando as buscas tanto no cartão quanto na prova

--- trunk/scacp/src/scacp/CartaoDAO.java
+++ trunk/scacp/src/scacp/CartaoDAO.java
@@ -6,6 +6,7 @@ package scacp;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
@@ -59,11 +60,41 @@ public class CartaoDAO {
         }
         // Passos para alteração de cartão
     }
-    public void buscarCartao(int numeroInscricao){
+    public void buscarCartao(int numeroInscricao, int idProva){
         Connection conexao = Conexao.getConexao();
+        Prova prova = new Prova();
+        Cartao cartao;
+        Cartao = new Cartao();
         try {
-            PreparedStatement st = conexao.prepareStatement("select from cartoes where numero_inscricao = ?");
+            PreparedStatement st = conexao.prepareStatement("select from cartoes where numero_inscricao = ? and id_prova = ?");
             st.setInt(1, numeroInscricao);
+            st.setInt(2, idProva);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                cartao.setIdProva(rs.getInt("id_prova"));
+                System.out.println("Identificação da prova:" + rs.getInt("id_prova"));
+                prova.setIdProva(rs.getInt("nome"));
+                System.out.println("Nome da prova:" + rs.getString("nome"));
+                prova.setIdProva(rs.getInt("tipo"));
+                System.out.println("Tipo de prova:" + rs.getInt ("tipo"));
+                prova.setIdProva(rs.getInt("quantidade_questoes"));
+                System.out.println("Quantidade de questões:" + rs.getInt ("quantidade_questoes"));
+                prova.setIdProva(rs.getInt("pontuacao_minima"));
+                System.out.println("Pontuação mínima:" + rs.getDouble("pontuacao_minima"));
+                prova.setIdProva(rs.getInt("pontuacao_maxima"));
+                System.out.println("Pontuação máxima:" + rs.getDouble("pontuacao_maxima"));
+                prova.setIdProva(rs.getInt("precisao_pontuacao"));
+                System.out.println("Precisão da pontuação:" + rs.getBoolean("precisao_pontuacao"));
+                prova.setIdProva(rs.getInt("incidencia_penalizacao"));
+                System.out.println("Incidência da penalização:" + rs.getBoolean("incidencia_penalizacao"));
+                prova.setIdProva(rs.getInt("proporcao_penalizacao"));
+                System.out.println("Proporção da penalização:" + rs.getInt("proporcao_penalizacao"));
+                prova.setIdProva(rs.getInt("gabarito"));
+                System.out.println("Gabarito:" + rs.getString("gabarito"));
+                
+                
+            }
+            
             st.execute();
             st.close();
             conexao.close();

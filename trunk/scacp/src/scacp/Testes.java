2f3e3e14b29860f77252a6b81fda7f711de0e42a
cleber.93cd <cleber.93cd@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-22 04:22:48 +0000

    Criando os painéis dos cartões.

--- trunk/scacp/src/scacp/Testes.java
+++ trunk/scacp/src/scacp/Testes.java
@@ -13,7 +13,25 @@ import javax.swing.JFrame;
 public class Testes {
     public static void main(String args[]){
         JFrame janela = new JFrame("Teste formulário!");
-        janela.add(new FormularioRespostas(1234567, 50, TipoProva.VERDADEIRO_FALSO));
+        janela.add(new FormularioQuestoes(9999990, 20, TipoProva.MULTIPLA_ESCOLHA));
+        //janela.add(new FormularioQuestoes(9999990, 20, TipoProva.VERDADEIRO_FALSO));
+        /*try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                System.out.println(info.getName());
+                if ("GTK+".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    //break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Scacp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Scacp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Scacp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Scacp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }*/
         janela.pack();
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setVisible(true);

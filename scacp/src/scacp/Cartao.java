4d040fcf1c0627e9c2a287fc17d07f339289157d
leticianb1 <leticianb1@9cf19b72-0c41-096d-4c9c-f759d7e51bfc>
2013-02-24 02:28:30 +0000

    Modificação do método validarInscricao, fazendo verificação do dado de entrada.

--- scacp/src/scacp/Cartao.java
+++ scacp/src/scacp/Cartao.java
@@ -5,21 +5,21 @@
 package scacp;
 
 import java.math.BigDecimal;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author cleber
  */
 public class Cartao {
+
     private int numeroInscricao;
-    private int tipoProva;
     private int idProva;
     private String marcacao;
     private double nota;
 
     public Cartao(int numeroInscricao, int tipoProva, String marcacao, double nota) {
         this.numeroInscricao = numeroInscricao;
-        this.tipoProva = tipoProva;
         this.marcacao = marcacao;
         this.nota = nota;
     }
@@ -27,21 +27,12 @@ public class Cartao {
     public Cartao() {
     }
 
-    
     public int getNumeroInscricao() {
         return numeroInscricao;
     }
 
     public void setNumeroInscricao(int numeroInscricao) {
         this.numeroInscricao = numeroInscricao;
-    }
-
-    public int getTipoProva() {
-        return tipoProva;
-    }
-
-    public void setTipoProva(int tipoProva) {
-        this.tipoProva = tipoProva;
     }
 
     public int getIdProva() {
@@ -63,91 +54,103 @@ public class Cartao {
     public double getNota() {
         return nota;
     }
-
+    
     public void setNota(double nota) {
         this.nota = nota;
     }
-    
-    
-    
-    public int validarInscricao(int numeroInscricao){
-        int tamanhoVetor = 7 ;
-        int contador, somador = 0, digitoVerificador;
-        int  vetorNumeroInscricao[] = new int[tamanhoVetor];
-        
-        //laço para guardar o número de inscrição em um vetor
-        for(contador=tamanhoVetor-1; contador>=0; contador--){ 
-               vetorNumeroInscricao[contador] = numeroInscricao%10;
-               numeroInscricao = numeroInscricao/10;
-            }
-        
-        for(contador=0; contador<tamanhoVetor-1; contador++){
-                somador += vetorNumeroInscricao[contador]*(tamanhoVetor-contador);
-            }
 
-	if((somador%11==0)||(somador%11==10)){
-	        digitoVerificador=0;
+    public static boolean validarInscricao(int numeroInscricao) {
+        String stringNumeroInscricao = String.format("%d", numeroInscricao);
+        if (stringNumeroInscricao.length() > 7) {
+            JOptionPane.showMessageDialog(null, "O número de inscrição não é válido");
+            return false;
+        }
+        if (stringNumeroInscricao.length() < 7) {
+            switch (7 - stringNumeroInscricao.length()) {
+                case 1:
+                    stringNumeroInscricao = String.format("%01d", numeroInscricao);
+                case 2:
+                    stringNumeroInscricao = String.format("%02d", numeroInscricao);
+                case 3:
+                    stringNumeroInscricao = String.format("%03d", numeroInscricao);
+                case 4:
+                    stringNumeroInscricao = String.format("%04d", numeroInscricao);
+                case 5:
+                    stringNumeroInscricao = String.format("%05d", numeroInscricao);
+                case 6:
+                    stringNumeroInscricao = String.format("%06d", numeroInscricao);
+
             }
+        }
+        System.out.println(stringNumeroInscricao);
+
+        int tamanhoDaInscricao = 7;
+        int contador, somador = 0, numero, digitoVerificador;
+        char cDigito;
+        for (contador = 0; contador < tamanhoDaInscricao - 2; contador++) {
+
+            cDigito = stringNumeroInscricao.charAt(contador);
+            numero = Character.getNumericValue(cDigito);
+            
+            somador += numero * (tamanhoDaInscricao - contador);
+        }
+
+        if ((somador % 11 == 0) || (somador % 11 == 10)) {
+            digitoVerificador = 0;
+        } 
         else {
-                digitoVerificador = 11 - somador%11;
-            }
+            digitoVerificador = 11 - somador % 11;
+        }
+        cDigito = stringNumeroInscricao.charAt(contador);
+        numero = Character.getNumericValue(cDigito);
         
-        return digitoVerificador;
+        if (digitoVerificador == numero) {
+            return true;
+        } else {
+            return false;
+        }
     }
-   
- public double ajustarPrecisao(double numero, int precisaoPontuacao){  
-     
-    int decimalPlace = precisaoPontuacao;  
-    BigDecimal formatador = new BigDecimal(numero);  
-    formatador = formatador.setScale(decimalPlace,BigDecimal.ROUND_HALF_UP);  
-    numero = formatador.doubleValue();  
-   return numero;
-    }  
+    
 
     
-    public double calcularNota(Prova prova){
-     String stringMarcacao = getMarcacao();
-      String stringGabarito = prova.getGabarito();
-      
-      int contador, questoesIncorretas = 0, questoesCorretas = 0, questoesBranco = 0;
-      double notaFinal;
-      //Contagem de questoes corretas, incorretas e em branco
-      for (contador = 0; contador < stringMarcacao.length(); contador ++){
-        if(stringGabarito.charAt(contador) == '#'){
-        questoesCorretas ++;
+
+
+
+    public void calcularNota(Prova prova) {
+        String stringMarcacao = getMarcacao();
+        String stringGabarito = prova.getGabarito();
+
+        int contador, questoesIncorretas = 0, questoesCorretas = 0, questoesBranco = 0;
+        double notaFinal;
+        //Contagem de questoes corretas, incorretas e em branco
+        for (contador = 0; contador < stringMarcacao.length(); contador++) {
+            if (stringGabarito.charAt(contador) == '#') {
+                questoesCorretas++;
+            } else if (stringMarcacao.charAt(contador) == '*') {
+                questoesBranco++;
+            } else if (stringMarcacao.charAt(contador) != stringGabarito.charAt(contador) || stringMarcacao.charAt(contador) == '#') {
+                questoesIncorretas++;
+            } else if (stringMarcacao.charAt(contador) == stringGabarito.charAt(contador)) {
+                questoesCorretas++;
+            } else {
+            }
         }
-        else if(stringMarcacao.charAt(contador) == '*'){
-        questoesBranco ++;
-        }
-        else if(stringMarcacao.charAt(contador) != stringGabarito.charAt(contador)|| stringMarcacao.charAt(contador) == '#'){
-        questoesIncorretas ++;
-        }
-      
-        else if (stringMarcacao.charAt(contador) == stringGabarito.charAt(contador)){
-        questoesCorretas ++;
-        }
-        else {
-        }
-      }
-       
+
         // cálculo da nota
-        if(prova.getIncidenciaPenalizacao()){
-        double fator;
-        fator = (prova.getProporcaoPenalizacao() * questoesCorretas - questoesIncorretas)/prova.getProporcaoPenalizacao();
-        
-        if(fator<0){ fator = 0;}
-        
-        notaFinal = prova.getPontuacaoMinima() + (( prova.getPontuacaoMaxima() - prova.getPontuacaoMinima())/ prova.getQuantidadeQuestoes()) * fator ;
-        }
-        else{
-         notaFinal = prova.getPontuacaoMinima() + ((  prova.getPontuacaoMaxima() - prova.getPontuacaoMinima())/ prova.getQuantidadeQuestoes()) * questoesCorretas ;
-        }
-        
-     return ajustarPrecisao(notaFinal, prova.getPrecisaoPontuacao());
-     }
+        if (prova.getIncidenciaPenalizacao()) {
+            double fator;
+            fator = (prova.getProporcaoPenalizacao() * questoesCorretas - questoesIncorretas) / prova.getProporcaoPenalizacao();
 
-    public static void main(String[] args) {
+            if (fator < 0) {
+                fator = 0;
+            }
+
+            notaFinal = prova.getPontuacaoMinima() + ((prova.getPontuacaoMaxima() - prova.getPontuacaoMinima()) / prova.getQuantidadeQuestoes()) * fator;
+        } else {
+            notaFinal = prova.getPontuacaoMinima() + ((prova.getPontuacaoMaxima() - prova.getPontuacaoMinima()) / prova.getQuantidadeQuestoes()) * questoesCorretas;
+        }
 
     }
-    }
+}
+
 
